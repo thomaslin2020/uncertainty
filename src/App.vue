33aab8d562d7e233f0334a616ee64cafbbf73fe2
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-05-09 18:04:42 -0400

    updated files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,143 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div class="main-container">
+            <div id="nav">
+                <b-navbar id="nav_bar" toggleable="md" type="light" variant="light">
+                    <b-navbar-brand to="/" style="padding-top: 0; padding-bottom: 0; margin-left: 0.375rem"><img
+                            src="@/assets/icon.png"
+                            style="max-width: 50px;" alt="Logo"/></b-navbar-brand>
+                    <b-navbar-toggle id="collapse-button" target="nav-collapse"></b-navbar-toggle>
+                    <b-collapse id="nav-collapse" is-nav>
+                        <b-navbar-nav class="ml-auto">
+                            <b-nav-item to="/home">Home</b-nav-item>
+                            <b-nav-item to="/about">About</b-nav-item>
+                            <b-nav-item to="/quick_guide">Quick Guide</b-nav-item>
+                            <b-nav-item to="/tools">Tools</b-nav-item>
+                            <b-nav-item to="/documentation">Documentation</b-nav-item>
+                            <b-nav-item to="#">Resources</b-nav-item>
+                        </b-navbar-nav>
+                    </b-collapse>
+                </b-navbar>
+            </div>
+            <br>
+            <div class="parent">
+                <div class="background-layer">
+                    <br>
+                    <router-view/>
+                    <br>
+                </div>
+            </div>
+        </div>
+        <footer class="footer page-footer font-small unique-color-dark ">
+            <div class="footer-copyright text-center py-3">
+                <div class="footer-text-container">
+                    <div style="width: 80%; vertical-align: center;">
+                        <div style="display: inline-flex; flex-direction: row;">
+                            <a class="media-button">
+                                <font-awesome-icon class="fa fa-wechat" :icon="{ prefix: 'fab', iconName: 'weixin' }"/>
+                            </a>
+                            <a class="media-button">
+                                <font-awesome-icon class="fa fa-instagram"
+                                                   :icon="{ prefix: 'fab', iconName: 'instagram' }"/>
+                            </a>
+                            <a class="media-button">
+                                <font-awesome-icon class="fa fa-github" :icon="{ prefix: 'fab', iconName: 'github' }"/>
+                            </a>
+                            <a class="media-button">
+                                <font-awesome-icon class="fa fa-mail" icon="envelope"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    .fa {
+        font-size: 30px;
+        text-align: center;
+        text-decoration: none;
+    }
 
-#nav {
-  padding: 30px;
-}
+    .fa:hover {
+        opacity: 0.7;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        background: linear-gradient(45deg, #00A8C5, #ecff7e);
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav_bar {
+        z-index: 10;
+        position: sticky;
+        top: 0;
+        height: 100%;
+        width: 100%;
+    }
+
+    .navbar-brand {
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
+    .nav-item {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .html {
+        height: 100%;
+    }
+
+    .main-container {
+        min-height: 100vh;
+    }
+
+    .background-layer {
+        background: rgba(255, 255, 255, 0.43);
+        width: 80%;
+        max-width: 80%;
+        border-radius: 10px
+    }
+
+    .parent {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .footer {
+        background-color: rgba(248, 249, 250, 0.8);
+        height: 60px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .footer-text-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .media-button {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import BootstrapVue from "bootstrap-vue"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faGithub, faInstagram, faWeixin} from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
+library.add(faGithub, faInstagram, faWeixin, faEnvelope)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
