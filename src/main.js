18dd3a48e8b7dcb1e6e3839a45496f144363ba0f
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-05-10 16:30:35 -0400

    updated files

--- src/main.js
+++ src/main.js
@@ -8,10 +8,13 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {faGithub, faInstagram, faWeixin} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import VueTableDynamic from 'vue-table-dynamic'
 
 library.add(faGithub, faInstagram, faWeixin, faEnvelope)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue);
+Vue.use(VueTableDynamic)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -1,13 +1,47 @@
 <template>
     <div>
         <h1>This is a Guide Page</h1>
-
+        <div style="width: 600px">
+            <vue-table-dynamic
+                    :params="params"
+                    @cell-change="onCellChange"
+                    ref="table"
+            >
+            </vue-table-dynamic>
+        </div>
     </div>
 </template>
-
 <script>
+    import VueTableDynamic from 'vue-table-dynamic'
+
     export default {
-        name: "Guide"
+        name: "Guide",
+        components: {VueTableDynamic},
+        data() {
+            return {
+                params: {
+                    data: [
+                        ['Index', 'Data1', 'Data2', 'Data3'],
+                        [1, 'b3ba90', '7c95f7', '9a3853'],
+                        [2, 'ec0b78', 'ba045d', 'ecf03c'],
+                        [3, '63788d', 'a8c325', 'aab418']
+                    ],
+                    header: 'row',
+                    stripe: true,
+                    edit: {
+                        row: [1],
+                        column: [1],
+                        cell: [[-1, -1]]
+                    }
+                }
+            }
+        },
+        methods: {
+            onCellChange(rowIndex, columnIndex, data) {
+                console.log('onCellChange: ', rowIndex, columnIndex, data)
+                console.log('table data: ', this.$refs.table.getData())
+            }
+        },
     }
 </script>
 
