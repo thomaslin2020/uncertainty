eab9404ecebb5cbdeb60ad15a5426e78a132dec9
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-05-11 18:05:48 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     outputDir: 'dist',
+<<<<<<< HEAD
     publicPath: process.env.NODE_ENV === 'production' ? 'https://uncertainty-calculator.herokuapp.com/' : '/'
+=======
+    baseUrl: IS_PRODUCTION ? 'https://uncertainty-calculator.herokuapp.com/' : '/',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/uncertainty/'
+        : '/'
+>>>>>>> parent of 498217e... Update vue.config.js
 }
